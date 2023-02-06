d7eb92209d477ea5d89d8d9cd0631762bc5fe25e
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-02-06 23:24:17 +0530

    router changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import './App.css'
 import Side from './Components/Sidepanel/Side'
 import Tasks from './Components/TaskList/Tasks'
 import Myday from './Components/MyDay/Myday'
-import { BrowserRouter as Route,Router } from 'react-router-dom'
+import { BrowserRouter as Router ,Route } from 'react-router-dom';
 function App() {
   
   return (
     <div className="App">
       <Router>
         <Side/>
-        <Route exact path="/"><Myday/></Route>
-        <Route path="/tasks"><Tasks/></Route>
-      </Router>
+        <Route path="/" component= {Myday}/>
+        <Route path="/tasks" component = {Tasks} />
+        </Router>
     </div>
   )
 }

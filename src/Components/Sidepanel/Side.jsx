90294a12ec0fb78a19f81210639504d6b517a852
SheshankM <108621304+SheshankM@users.noreply.github.com>
2023-02-21 22:14:05 +0530

    update styles

--- src/Components/Sidepanel/Side.jsx
+++ src/Components/Sidepanel/Side.jsx
@@ -4,21 +4,15 @@ import { IoMdStarOutline } from 'react-icons/io';
 import { FaTasks, FaRegCalendarAlt } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 function Side() {
-  const linkStyles =({isActive})=>{
-    return {
-      textDecoration: 'none',
-      backgroundColor: isActive?'#6a6a6a':'none'
-    }
-  } 
   return (
     <div className='sidebar'>
       <div className="sidebarWrapper">
         <h2 className='heading'>Todo List</h2>
         <ul className='list-names'>
-          <NavLink to='/' style={linkStyles}><li><RiSunFill className='icon' /><span>My Day</span></li></NavLink>
-          <NavLink to='important' style={linkStyles}><li><IoMdStarOutline className='icon'/> Important</li></NavLink>
-          <NavLink to='planned' style={linkStyles}><li><FaRegCalendarAlt className='icon'/>Planned</li></NavLink>
-          <NavLink to='tasks' style={linkStyles}><li><FaTasks className='icon'/>Tasks</li></NavLink>
+          <NavLink to='/' className={(t)=>t.isActive?'active':''}><li><RiSunFill className='icon' /><span>My Day</span></li></NavLink>
+          <NavLink to='important' className={(t)=>t.isActive?'active':''}><li><IoMdStarOutline className='icon star'/> Important</li></NavLink>
+          <NavLink to='planned' className={(t)=>t.isActive?'active':''}><li><FaRegCalendarAlt className='icon'/>Planned</li></NavLink>
+          <NavLink to='tasks' className={(t)=>t.isActive?'active':''}><li><FaTasks className='icon'/>Tasks</li></NavLink>
         </ul>
       </div>
     </div>
